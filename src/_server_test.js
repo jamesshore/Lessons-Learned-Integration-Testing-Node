6ba80d95652a44ae230958a33b8abca04f36dfe1
James Shore <jshore@jamesshore.com>
2012-08-23 12:21:59 -0700

    Factored out setUp() and tearDown() for added cleanliness

--- src/_server_test.js
+++ src/_server_test.js
@@ -5,27 +5,35 @@
 	var server = require("./server.js");
 	var http = require("http");
 
-	exports.test_serverReturnsHelloWorld = function (test) {
+	exports.setUp = function(done) {
 		server.start(8080);
-		simpleGet("http://localhost:8080", function (response, responseText) {
+		done();
+	};
+
+	exports.tearDown = function(done) {
+		server.stop(function() {
+			done();
+		});
+	};
+
+	exports.test_serverReturnsHelloWorld = function(test) {
+		simpleGet("http://localhost:8080", function(response, responseText) {
 			test.equals(200, response.statusCode, "status code");
 			test.equals("Hello World", responseText, "response text");
-			server.stop(function () {
-				test.done();
-			});
+			test.done();
 		});
 	};
 
 	function simpleGet(url, callback) {
 		var request = http.get("http://localhost:8080");
-		request.on("response", function (response) {
+		request.on("response", function(response) {
 			var responseText = "";
 			response.setEncoding("utf8");
 
-			response.on("data", function (chunk) {
+			response.on("data", function(chunk) {
 				responseText += chunk;
 			});
-			response.on("end", function () {
+			response.on("end", function() {
 				callback(response, responseText);
 			});
 		});
