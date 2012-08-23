d998740d986e7542c2e9cb62d776cdd0fcb2b1fd
James Shore <jshore@jamesshore.com>
2012-08-23 12:08:08 -0700

    Copied HTTP test from Live series

---
+++ src/_server_test.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
+"use strict";
+
+var server = require("./server.js");
+var http = require("http");
+
+exports.test_serverReturnsHelloWorld = function (test) {
+	server.start(8080);
+	var request = http.get("http://localhost:8080");
+	request.on("response", function (response) {
+		var receivedData = false;
+		response.setEncoding("utf8");
+
+		test.equals(200, response.statusCode, "status code");
+		response.on("data", function (chunk) {
+			receivedData = true;
+			test.equals("Hello World", chunk, "response text");
+		});
+		response.on("end", function () {
+			test.ok(receivedData, "should have received response data");
+			server.stop(function () {
+				test.done();
+			});
+		});
+	});
+};
