abbbafd201187658bc90832fb5653aff85d307d1
James Shore <jshore@jamesshore.com>
2012-08-30 10:56:50 -0700

    Added event emitter example

---
+++ src/event_emitter_pattern.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
+"use strict";
+
+var http = require("http");
+
+http.get("http://google.com", function(response) {
+	response.setEncoding("utf8");
+
+	response.on("data", function(chunk) {
+		console.log("** Received: " + chunk);
+	});
+
+	response.on("end", function() {
+		console.log("** End of response.")
+	});
+});
